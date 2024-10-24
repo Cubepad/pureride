362ba2755cbf250f50d0cfece2eab5a65fa5d27f
David Olaniyan <davideniola108@gmail.com>
2024-10-24 07:55:51 +0200

    Added the footer.css and added footer with phone number, email and contact details. Also added copyright text at the bottom and used javascript to ensure the year updates. Updated the formatting for the header

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 window.addEventListener('scroll', () => {
   const header = document.querySelector('.header');
-  if (window.scrollY > 0) {
+  if (window.scrollY > 1) {
       header.classList.add('scrolled');
   } else {
       header.classList.remove('scrolled');
   }
 });
+
+document.getElementById("year").textContent = new Date().getFullYear();
